2b3c556187615f9a6f1ec8ff09d2b02e918062dc
Aneesh Vishwa Dorishetty <156919974+aneesh-vishwa@users.noreply.github.com>
2025-11-25 13:10:15 +0530

    Time: 3 ms (41.43%), Space: 43 MB (57%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.java
+++ 0264-ugly-number-ii/0264-ugly-number-ii.java
@@ -1,16 +1,32 @@
 class Solution {
     public int nthUglyNumber(int n) {
-        TreeSet<Long> set = new TreeSet<>();
-        set.add(1L);
-        long current_ugly = 1;
-        for(int i = 0 ; i < n - 1 ; i++){
-            current_ugly = set.pollFirst();
+        int[] nums = new int[n];
+        nums[0] = 1;
+        //2 , 3 , 5
+        int i_2 = 0;
+        int i_3 = 0;
+        int i_5 = 0;
 
-            set.add(current_ugly*2);
-            set.add(current_ugly*3);
-            set.add(current_ugly*5);
+        for(int i = 1 ; i < n ; i++){
+
+            int next_2 = nums[i_2] * 2;
+            int next_3 = nums[i_3] * 3;
+            int next_5 = nums[i_5] * 5;
+
+            int current = Math.min(next_2, Math.min(next_3, next_5));
+
+            nums[i] = current;
+
+            if(current % 2 == 0){
+                i_2++;
+            }
+            if(current % 3 == 0){
+                i_3++;
+            }
+            if(current % 5 == 0){
+                i_5++;
+            }
         }
-        long res = set.pollFirst();
-        return (int)res;
+        return nums[n - 1];
     }
 }
